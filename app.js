770bc34fb9c27e9e7cbb51c1b43a7fb14d3db5f2
vishnu uplenchwar <vishnuuplenchwar.professional@gmail.com>
2026-01-18 23:41:59 +0530

    fixed teh bug

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(cors({
             "https://gitvault-client.netlify.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Set-Cookie']
 }));
 
 app.use(express.json());

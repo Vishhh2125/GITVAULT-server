83e80f39fc8f82fe71d426142dd0249d61d4942c
vishnu uplenchwar <vishnuuplenchwar.professional@gmail.com>
2026-01-18 23:17:14 +0530

    add cors permission

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ if (!process.env.REPO_BASE_PATH) {
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173',
+            "https://gitvault-client.netlify.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
